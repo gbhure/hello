ba64206fe288a7e1632889a403c95f26b1e9ede2
Ganesh <ganesh.bhure1@gmail.com>
2021-07-24 03:56:59 +0000

    A committed in file.py

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 print('Python project started')
+print('new code added by A')
 x = 10
 y = 20
 res = x-y if x>y else y-x
-print(f'final output is: {res}')
+print(f'final output after operation is: {res}')
