cfc19b9ab0e6526b3e51af6b8d86ea24c0333298
gbhure <45383300+gbhure@users.noreply.github.com>
2021-08-14 10:54:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world from git code");
-        System.out.println("This is a new commit done..");
+        System.out.println("This is a change through dev branch1 done..");
     }
 }
